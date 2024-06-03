85a6e9e2ffa60f052c3ef34fff6a6f6d9bfd9527
Harlan Williams <hrw@ku.edu>
2024-06-03 16:01:28 -0500

    Test request to catalog

--- R/main.R
+++ R/main.R
@@ -1,11 +1,10 @@
-library(curl)
+catalog_search <- function(queries) {
+  url_query <- encode_params(queries)
 
-arctosr_req <- function() {
-  res <- new_handle() |>
-    handle_setheaders("User-Agent"="ArctosR") |>
-    curl_fetch_memory(url = "https://arctos.database.museum/")
+  res <- curl::new_handle() |>
+    curl::handle_setheaders("User-Agent"="ArctosR/0.1.0") |>
+    curl::curl_fetch_memory(
+      url = sprintf("https://arctos.database.museum/component/api/v2/catalog.cfc?%s", url_query)
+    )
   res
 }
-
-res <- arctosr_req()
-str(res)
