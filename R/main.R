e34d5517ac779c8bdaee8aa299dc41a3421353a2
Harlan Williams <hrw@ku.edu>
2024-06-05 14:18:41 -0500

    clean up functions for making requests

--- R/main.R
+++ R/main.R
@@ -1,10 +1,14 @@
-catalog_search <- function(queries) {
-  url_query <- encode_params(queries)
+new_arctosr_handle <- function() {
+  h <- curl::new_handle() |>
+    curl::handle_setheaders("User-Agent"="ArctosR/0.1.0")
+}
 
-  res <- curl::new_handle() |>
-    curl::handle_setheaders("User-Agent"="ArctosR/0.1.0") |>
-    curl::curl_fetch_memory(
-      url = sprintf("https://arctos.database.museum/component/api/v2/catalog.cfc?%s", url_query)
-    )
-  res
+catalog_search <- function(queries) {
+  url <- build_authenticated_url("catalog.cfc", queries)
+  curl::curl_fetch_memory(new_arctosr_handle(), url = url)
+}
+
+catalog_about <- function() {
+  url <- build_url("catalog.cfc", list("method" = "about"))
+  curl::curl_fetch_memory(new_arctosr_handle(), url = url)
 }
