50b03dc90b2c45fd35fa504f37781b7a5716b66c
Harlan Williams <hrw@ku.edu>
2024-06-06 14:50:06 -0500

    add functions to return a json of the response

--- R/main.R
+++ R/main.R
@@ -1,14 +1,34 @@
-new_arctosr_handle <- function() {
-  h <- curl::new_handle() |>
-    curl::handle_setheaders("User-Agent"="ArctosR/0.1.0")
-}
+catalog_search_raw <- function(queries) {
+  queries$method <- "getCatalogData"
+  queries$queryformat <- "struct"
 
-catalog_search <- function(queries) {
   url <- build_authenticated_url("catalog.cfc", queries)
   curl::curl_fetch_memory(new_arctosr_handle(), url = url)
 }
 
-catalog_about <- function() {
+catalog_about_raw <- function() {
   url <- build_url("catalog.cfc", list("method" = "about"))
   curl::curl_fetch_memory(new_arctosr_handle(), url = url)
+}
+
+catalog_search <- function(...) {
+  r <- catalog_search_raw(list(...))
+
+  if (r$status_code == 200) {
+    jsonlite::parse_json(rawToChar(r$content))
+  } else {
+    r
+    # report error
+  }
+}
+
+catalog_about <- function() {
+  r <- catalog_about_raw()
+
+  if (r$status_code == 200) {
+    jsonlite::parse_json(rawToChar(r$content))
+  } else {
+    r
+    # report error
+  }
 }
