82947ee495d620c976b73201359d33da118cd149
Harlan Williams <hrw@ku.edu>
2024-06-10 12:28:01 -0500

    fix var name

--- R/main.R
+++ R/main.R
@@ -15,13 +15,13 @@ catalog_search <- function(...) {
   response <- catalog_search_raw(list(...))
   json <- jsonlite::fromJSON(rawToChar(response$content))
 
-  if (r$status_code == 200) {
+  if (response$status_code == 200) {
     json
   } else {
     if (!(is.null(json$error) || is.null(json$Message))) {
-      stop(sprintf("HTTP %d %s: %s", r$status_code, json$error, json$Message))
+      stop(sprintf("HTTP %d %s: %s", response$status_code, json$error, json$Message))
     } else {
-      stop(sprintf("HTTP %d response from Arctos", r$status_code))
+      stop(sprintf("HTTP %d response from Arctos", response$status_code))
     }
   }
 }
@@ -30,13 +30,13 @@ catalog_about <- function() {
   response <- catalog_about_raw()
   json <- jsonlite::fromJSON(rawToChar(response$content))
 
-  if (r$status_code == 200) {
+  if (response$status_code == 200) {
     json
   } else {
     if (!(is.null(json$error) || is.null(json$Message))) {
-      stop(sprintf("HTTP %d %s: %s", r$status_code, json$error, json$Message))
+      stop(sprintf("HTTP %d %s: %s", response$status_code, json$error, json$Message))
     } else {
-      stop(sprintf("HTTP %d response from Arctos", r$status_code))
+      stop(sprintf("HTTP %d response from Arctos", response$status_code))
     }
   }
 }
