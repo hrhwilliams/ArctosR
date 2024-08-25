c2f56ca21fd1fd7278ea68cf8cc96336091a62e3
Harlan Williams <hrw@ku.edu>
2024-08-25 17:08:50 -0500

    add doc file

---
+++ R/arctosr_doc.R
@@ -0,0 +1,15 @@
+#' ArctosR: An Interface to the Arctos Database
+#'
+#' @description
+#' The `ArctosR` package provides a set of functions to help users
+#' perform requests to the Arctos API to download data. It provides a set of
+#' builder classes for performing complex requests, as well as a set of simple
+#' functions for automating many common requests and workflows.
+#'
+#' @section Functions in ArctosR:
+#' \code{\link{get_query_parameters}}, \code{\link{get_result_parameters}},
+#' \code{\link{get_record_count}}, \code{\link{get_records}},
+#' \code{\link{response_data}}, \code{\link{save_response_rds}},
+#' \code{\link{read_response_rds}}, \code{\link{save_response_csv}}
+#'
+"_PACKAGE"
