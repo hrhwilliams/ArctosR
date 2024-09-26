0245af85d2eea4b1ca985dc8de5d92f5b823ba64
Harlan Williams <hrw@ku.edu>
2024-09-26 14:36:04 -0500

    get codecov to run

--- tests/testthat/test-backend.R
+++ tests/testthat/test-backend.R
@@ -7,6 +7,14 @@ test_that("build_url", {
   )
 })
 
+test_that("urlencode list", {
+  encode_list(c(hi = "something", "bye", NULL), collapse=",")
+  testthat::expect_equal(encode_list(c(one = "one", two = "two"), collapse='&'),
+                         "one=one&two=two")
+  testthat::expect_equal(encode_list(c("one", "two", "three"), collapse='&'),
+                         "one&two&three")
+})
+
 test_that("file extension", {
   testthat::expect_equal(file_extension("example.csv"),  "csv")
   testthat::expect_equal(file_extension("example.json"), "json")
