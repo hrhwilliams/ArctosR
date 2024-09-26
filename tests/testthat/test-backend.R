ca13ce456346c1293e22c4d319ce54ad979e3e08
Harlan Williams <hrw@ku.edu>
2024-09-26 14:26:21 -0500

    add more tests

--- tests/testthat/test-backend.R
+++ tests/testthat/test-backend.R
@@ -6,3 +6,17 @@ test_that("build_url", {
     "https://arctos.database.museum/component/api/v2/catalog.cfc?method=getCatalogData&queryformat=struct&scientific_name=Canis%20lupus&guid_prefix=MSB%3AMamm&length=50"
   )
 })
+
+test_that("file extension", {
+  testthat::expect_equal(file_extension("example.csv"),  "csv")
+  testthat::expect_equal(file_extension("example.json"), "json")
+})
+
+test_that("file extension empty", {
+  testthat::expect_equal(file_extension("example"), NULL)
+})
+
+test_that("file name", {
+  testthat::expect_equal(file_name("example.csv"),  "example")
+  testthat::expect_equal(file_name("example.json"), "example")
+})
--- tests/testthat/test-frontend.R
+++ tests/testthat/test-frontend.R
@@ -5,14 +5,39 @@ test_that("get_records_no_cols", {
   query <- Query$new()
   response <- query$catalog_request_from_raw_response(raw_response)
 
+  testthat::expect_equal(response$was_success(), TRUE)
+  testthat::expect_equal(response$is_empty(), FALSE)
+
+  info <- response$to_list()
+  testthat::expect_equal(info$metadata$status_code, 200)
+  testthat::expect_equal(info$index_range, c(1, 50))
+
   df <- response_data(query)
   testthat::expect_s3_class(df, "data.frame")
+})
+
+test_that("get_records_no_cols concatenate", {
+  raw_response1 <- readRDS('test_request_no_cols.rds')
+  raw_response2 <- readRDS('test_request_no_cols_part2.rds')
+  query <- Query$new()
+  response1 <- query$catalog_request_from_raw_response(raw_response1)
+  response2 <- query$catalog_request_from_raw_response(raw_response2)
+
+  df <- response_data(query)
+  testthat::expect_equal(nrow(df), 100)
 })
 
 test_that("get_records_with_cols", {
   raw_response <- readRDS('test_request_with_cols.rds')
   query <- Query$new()
   response <- query$catalog_request_from_raw_response(raw_response)
+
+  testthat::expect_equal(response$was_success(), TRUE)
+  testthat::expect_equal(response$is_empty(), FALSE)
+
+  info <- response$to_list()
+  testthat::expect_equal(info$metadata$status_code, 200)
+  testthat::expect_equal(info$index_range, c(1, 50))
 
   df <- response_data(query)
 
