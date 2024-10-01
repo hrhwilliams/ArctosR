a2528f1caaf39f39e48031fdf92c9c09222d314b
marlonecobos <manubio13@gmail.com>
2024-10-01 11:33:32 -0500

    documentation of package expanded

--- R/arctosr_doc.R
+++ R/arctosr_doc.R
@@ -6,9 +6,14 @@
 #' builder classes for performing complex requests, as well as a set of simple
 #' functions for automating many common requests and workflows.
 #'
+#' @section About Arctos:
+#' Arctos is community and an online collection management information system.
+#' Arctos is a consortium of museums and organizations that collaborate to serve
+#' data on over 5 million records from natural and cultural history collections.
 #' The web interface to the Arctos database can be found at
 #' https://arctos.database.museum/. For more information about Arctos,
-#' see https://arctosdb.org/about/.
+#' see https://arctosdb.org/about/, and
+#' Cicero et al. (2024) <doi:10.1371/journal.pone.0296478>.
 #'
 #' @section Functions in ArctosR:
 #' \code{\link{get_query_parameters}}, \code{\link{get_result_parameters}},
