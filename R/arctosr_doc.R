a2bc0d27ee180980bbaf481d0d5011c86f2d7cd9
Harlan Williams <hrw@ku.edu>
2025-08-15 15:25:52 -0500

    improve package description

--- R/arctosr_doc.R
+++ R/arctosr_doc.R
@@ -7,21 +7,21 @@
 #' functions for automating many common requests and workflows.
 #'
 #' @section About Arctos:
-#' Arctos is community and an online collection management information system.
-#' Arctos is a consortium of museums and organizations that collaborate to serve
-#' data on over 5 million records from natural and cultural history collections.
-#' Arctos integrates access to information from diverse disciplines:
-#' anthropology, botany, entomology, ethnology, herpetology, geology,
-#' ichthyology, mammalogy, mineralogy, ornithology, paleontology,
-#' parasitology as well as archival and cultural collections.
-#' The web interface to the Arctos database can be found at
-#' https://arctos.database.museum/. For more information about Arctos,
-#' see https://arctosdb.org/about/, and
-#' Cicero et al. (2024) <doi:10.1371/journal.pone.0296478>.
+#' Arctos is a collection management information system serving over 5 million
+#' records from natural and cultural history collections. Arctos integrates
+#' access to collections from disciplines such as anthropology, botany,
+#' entomology, ethnology, herpetology, geology, ichthyology, mammalogy,
+#' mineralogy, ornithology, paleontology, parasitology, as well as archival and
+#' cultural collections. The Arctos database is accessible through a web
+#' interface at <https://arctos.database.museum/> More information about Arctos,
+#' can be found at <https://arctosdb.org/about/>, and in Cicero et al. (2024)
+#' <doi:10.1371/journal.pone.0296478>.
 #'
 #' @section Functions in ArctosR:
 #' \code{\link{get_query_parameters}}, \code{\link{get_result_parameters}},
 #' \code{\link{get_record_count}}, \code{\link{get_records}},
+#' \code{\link{check_for_status}}, \code{\link{get_error_response}},
+#' \code{\link{get_last_response_url}},
 #' \code{\link{response_data}}, \code{\link{save_response_rds}},
 #' \code{\link{read_response_rds}}, \code{\link{save_response_csv}},
 #' \code{\link{expand_column}}
